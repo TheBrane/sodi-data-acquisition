bea3391d90b7a4f2eeaedfcb05d05a7b583d3e84
Aisha Pectyo <aisha.mahmoud@gmail.com>
2017-08-08 19:03:07 -0500

    Create basicpull_bot.py

---
+++ basicpull_bot.py
@@ -0,0 +1,6 @@
+""" Code to do a basic Reddit pull using the hottest topics on a subreddit.  Simple bot to track posts
+Aisha Pectyo, 8/8/2017"""
+
+#Import Libraries
+import praw
+from sklearn.datasets import fetch_20newsgroups
