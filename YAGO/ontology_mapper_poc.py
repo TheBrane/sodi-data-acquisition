5b2541ad36cd6926d9ea541dc0f2d37355aae9a8
rish-r <70616667+rish-r@users.noreply.github.com>
2020-09-20 18:59:05 -0700

    Create ontology_mapper_poc.py
    inital commit

---
+++ YAGO/ontology_mapper_poc.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 15 07:37:02 2020
+
+@author: rish-r
+"""
+
+# Proof-of-concept for a general purpose ontology mapper
+# Extract from YAGO and DBpedia and load into ArangoDB KG
+
+
+# Query SPARQL databases, return formatted RDF triples 
+def triples_generator(endpoint_url,sparql):
+    """
+    Parameters
+    ----------
+    endpoint_url : TYPE
+        DESCRIPTION.
+    sparql : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+# Convert to standardized RDF vocabulary
+#def vocab_mapper():
+
+# Convert to custom ontology by lookup on file or table    
+def ontology_mapper():
+    
+# Create nodes in ArangoDB
+def create_nodes():    
+
+# Create edges in ArangoDB
+def create_edges():
+
+    
+    
