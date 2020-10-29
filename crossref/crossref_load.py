b78ca0df3b7ab961b01bcaa151f9f0f9d92df030
rish-r <70616667+rish-r@users.noreply.github.com>
2020-10-28 19:36:49 -0700

    Update crossref_load.py
    refactoring

--- crossref/crossref_load.py
+++ crossref/crossref_load.py
@@ -5,47 +5,18 @@ Created on Sat Oct 17 2020
 @author: rish-r
 """
 # Extract from Crossref JSON files, Crossref API, load into ArangoDB KG
+from config import *
 from initialize_arango import *
 from ontology_mapper import *
+
 from arango import ArangoClient, ArangoServerError, DocumentInsertError
 import json
 import requests
+import time
 from pathlib import Path
 
-subject_type = 'http://schema.org/CreativeWork'
 
-def search_work(DOI, uri=None):
-    """
-    Parameters
-    ----------
-    DOI : str
-        Digital Object Identifier, is a string of numbers, letters and symbols
-        used to permanently identify an article or document and link to it on the web.
-    uri :  str, optional
-        Resource identifier for RDF subject or object, typically URL.
-        The default is None.
-    Returns
-    -------
-    ID : str
-        Document _id from 'Work' collection.
-    """
-    try:
-        DOI_upper = str(DOI).upper()
-        db = intialize_client()
-        # Get the AQL API wrapper.
-        aql = db.aql
-        collec = db.collection('Works')
-        aq='FOR doc IN Works FILTER doc.DOI=="'+str(DOI_upper)+'" OR doc.URI=="'+str(uri)+'" RETURN doc._id'
-        # Execute AQL query to match existing records in DB
-        ID = list(db.aql.execute(aq))
-        if ID:
-            return ID[0]
-        else:
-            print("Document not found")
-            return 0
-    except IndexError as e:
-        print(e)
-        return 0
+
 
 def search_doc(uri, collection_name, label=None):
     """
@@ -86,14 +57,14 @@ def load_crossref_data(data):
     Parameters
     ----------
     data : JSON
-        JSON file format.
+        A single corssref JSON file, containing a work.
     Returns
     -------
     ID : str
         ArangoDB document _id(also called handle), which uniquely identifies
         the document across all collections within a DB.
     """
-    # DOI is case insensitive, default to UPPER
+    ### standardize DOI to uppercase, it is case-insensitive
     DOI = data.get('message').get('DOI').upper()
     URL = data.get('message').get('URL')
     title = str(data.get('message').get('title'))[1:-1]
@@ -102,7 +73,8 @@ def load_crossref_data(data):
     # additional data passed as fields dict
     fields = {'DOI': DOI, 'title': title, 'subject': subject,
               'work_type': work_type, 'data': data.get('message')}
-    ID = create_nodes(DOI, subject_type, fields)
+    ID = create_works(DOI, subject_type_work, fields)
+    memo_DOI_ID.update({DOI: ID})
     return ID
 
 
@@ -125,22 +97,21 @@ def load_crossref_resp(crossref_url, parameters=None):
     response = response.json().get('message').get('items')
     #print(response.json().get('message').get('items'))
     for record in response:
+        ### standardize DOI to uppercase, it is case-insensitive
         DOI = record.get('DOI').upper()
         title = str(record.get('title'))[1:-1]
         fields = {'DOI': DOI, 'title': title, 'data': record}
-        ID = create_nodes(DOI, subject_type, fields)
+        print(DOI, subject_type, fields)
+        ID = create_works(DOI, subject_type, fields)
+        memo_DOI_ID.update({DOI: ID})
         record_num += 1
     return record_num
 
     
 def main():
+    start = time.time()
     #intialize_database(db_name, user, password)
-
-    predicate_citation = 'https://schema.org/citation'
-    predicate_author = 'https://schema.org/author'
-
-    json_folder = Path("D:\SODI\Proj\AlphaGO_graph")
-    crossref_url = 'https://api.crossref.org/works'
+    #intialize_memos()
 
     #load from crossref JSON files
     file_num = 0
@@ -151,6 +122,8 @@ def main():
         file_num += 1
         # print("created #", file_num)
     print("Total number of files:", file_num)
+    print(memo_DOI_ID)
+    json.dump(memo_DOI_ID, open("memo_DOI_ID.json", 'w'))
 
 
 
@@ -162,12 +135,19 @@ def main():
 #                           }
 # =============================================================================
 
-    #load from crossref API, random sample of 10 records
-    crossref_url ='http://api.crossref.org/works?sample=10'
-    parameters = {"select":"DOI,title"}
-    
-    rec = load_crossref_resp(crossref_url, parameters)
-    print('Total number of records:', rec)
+# =============================================================================
+#     #load from crossref API, random sample of 10 records
+#     crossref_url ='http://api.crossref.org/works?sample=10'
+#     parameters = {"select":"DOI,title"}
+#     
+#     rec_count = load_crossref_resp(crossref_url, parameters)
+#     print('Total number of records:', rec_count)
+# 
+# =============================================================================
 
+
+    json.dump(memo_TBD_DOI, open("memo_TBD_DOI.json", 'w'))
+    end = time.time()
+    print('crossref load, create works',end - start)
 if __name__ == "__main__":
     main()
